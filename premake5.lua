df95955893f890dfbed5d273c2554f5f9c8b1e1c
InfiniteC0re <inf.core0@gmail.com>
2022-07-15 00:53:12 +0500

    premake

---
+++ premake5.lua
@@ -0,0 +1,108 @@
+workspace "Deyo"
+	architecture "x64"
+	platforms "Windows"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}_%{cfg.platform}_%{cfg.architecture}"
+
+project "Deyo"
+	location "Deyo"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	postbuildcommands
+	{
+		("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Playground")
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"DEYO_PLATFORM_WINDOWS",
+			"DEYO_ENGINE",
+			"_WINDLL"
+		}
+
+	filter "configurations:Debug"
+		defines "DEYO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DEYO_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DEYO_DIST"
+		optimize "On"
+
+
+project "Playground"
+	location "Playground"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	links
+	{
+		"Deyo"
+	}
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Deyo/vendor/spdlog/include",
+		"Deyo/src"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"DEYO_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "DEYO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "DEYO_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DEYO_DIST"
+		optimize "On"
